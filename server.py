4bde7acc662b5af2bebe0f2673ee7150d9686f97
Kyle DeCarlo <kyle.decarlo@gmail.com>
2023-10-02 10:09:06 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -3,16 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def table():
-    users = [
-        {"first_name": "Michael", "last_name": "Choi"},
-        {"first_name": "John", "last_name": "Supsupin"},
-        {"first_name": "Mark", "last_name": "Guillen"},
-        {"first_name": "KB", "last_name": "Tonel"},
-    ]
-    return render_template("table.html", users=users)
+
+@app.route('/play')
+def playl1():
+    return render_template('index.html',)
+
+@app.route('/play/<int:x>')
+def playlv2(x):
+    return render_template('index2.html', times=x)
+
+@app.route('/play/<int:x>/<color>')
+def playlv3(x, color):
+    return render_template('index3.html', times=x, color=color)
 
 
-if __name__ == "main":
+if __name__=="main":
     app.run(debug=True)
